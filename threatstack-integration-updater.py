705197957ce9d1cfae123da58def6f40cb85fd8e
Tom McLaughlin <tmclaugh@gmail.com>
2017-06-09 12:13:59 -0400

    Handle ZipFile uploads
    Using FunctionS3Bucket and FunctionS3Key requires bucket permissions to
    access the object.  Setting FunctionZipFile to a URL will download the
    file and upload it.  Use this when the file is only available via URL
    and you don't have S3 access permissions to the object.

--- threatstack-integration-updater.py
+++ threatstack-integration-updater.py
@@ -1,9 +1,17 @@
 #!env python
 import boto3
+import io
 import json
 import logging
 from logging.config import fileConfig
 import os
+import sys
+import zipfile
+
+try:
+    from urllib.request import urlopen
+except:
+    from urllib import urlopen
 
 dirname = os.path.dirname(__file__)
 logging_conf = os.path.join(dirname, 'logging.conf')
@@ -12,24 +20,54 @@ if os.environ.get('TS_DEBUG'):
     logging.root.setLevel(level=logging.DEBUG)
 _logger = logging.getLogger(__name__)
 
+def _fetch_zip_file(url):
+    '''
+    Fetch a file from a URL and return a file-like object.
+    '''
+    u = urlopen(url)
+    zip_file = io.BytesIO(u.read())
+
+    if zipfile.is_zipfile(zip_file):
+        zip_file.seek(0)
+    else:
+        raise Exception('Not valid zipfile.')
+
+    return zip_file
+
 def handler(event, context):
     '''
     Update a lambda function.
     '''
 
     function_name = os.environ.get('FUNCTION_NAME') or event.get('FunctionName')
+    function_zip_file_url = os.environ.get('FUNCTION_ZIP_FILE_URL') or event.get('FunctionZipFileUrl')
     function_s3_bucket = os.environ.get('FUNCTION_S3_BUCKET') or event.get('FunctionS3Bucket')
     function_s3_key = os.environ.get('FUNCTION_S3_KEY') or event.get('FunctionS3Key')
 
-    _logger.info(event)
+    _logger.info(json.dumps(event))
+
+    # S3{Bucket,Key} require S3 permissions/IAM policy for access. If using a
+    # URL then we must fetch and get bytes.
+    kwargs = {
+        'FunctionName': function_name,
+        'Publish': True
+    }
+
+    if function_zip_file_url:
+        function_zip_file_bytes = _fetch_zip_file(function_zip_file_url)
+        additional_args = {
+            'ZipFile': function_zip_file_bytes.read()
+        }
+        kwargs.update(additional_args)
+    else:
+        additional_args = {
+            'S3Bucket': function_s3_bucket,
+            'S3Key': function_s3_key,
+        }
+        kwargs.update(additional_args)
 
     lambda_client = boto3.client('lambda')
-    resp = lambda_client.update_function_code(
-        FunctionName=function_name,
-        S3Bucket=function_s3_bucket,
-        S3Key=function_s3_key,
-        Publish=True
-    )
+    resp = lambda_client.update_function_code(**kwargs)
 
     _logger.info(json.dumps(resp))
 
