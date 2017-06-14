7d514886119912358fe6bf291f142b2acdb9672b
Tom McLaughlin <tmclaugh@gmail.com>
2017-06-14 11:30:28 -0400

    Add some comments to explain flow and logging

--- threatstack-integration-updater.py
+++ threatstack-integration-updater.py
@@ -21,12 +21,17 @@ if os.environ.get('TS_DEBUG'):
     logging.root.setLevel(level=logging.DEBUG)
 _logger = logging.getLogger(__name__)
 
+_logger.debug(sys.version)
+_logger.debug(sys.path)
+
 handler = cfn_resource.Resource()
 
 def _fetch_zip_file(url):
     '''
     Fetch a file from a URL and return a file-like object.
     '''
+
+    # We'll throw an exception if we don't receive a successful response.
     u = urlopen(url)
     zip_file = io.BytesIO(u.read())
 
@@ -42,13 +47,15 @@ def update_lambda(event, context):
     '''
     Update a lambda function.
     '''
+    _logger.info('CloudFormation event received: {}'.format(json.dumps(event)))
+
+    # get Properties keys sent from CFN.
+    # ref: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/crpg-ref-requests.html
     properties = event.get('ResourceProperties')
     function_name = properties.get('FunctionName')
     function_zip_file_url = properties.get('FunctionZipFileUrl')
     function_s3_bucket = properties.get('FunctionS3Bucket')
     function_s3_key = properties.get('FunctionS3Key')
-
-    _logger.info(json.dumps(event))
 
     # S3{Bucket,Key} require S3 permissions/IAM policy for access. If using a
     # URL then we must fetch and get bytes.
@@ -70,16 +77,23 @@ def update_lambda(event, context):
         }
         lambda_update_kwargs.update(additional_args)
 
+    # Update Lambda function
     lambda_client = boto3.client('lambda')
     lambda_update_resp = lambda_client.update_function_code(**lambda_update_kwargs)
 
+    # Construct our CFN response. cfn_resource will handle setting RequestId.
+    # ref: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/crpg-ref-responses.html
     cfn_resp = {
         'Status': 'SUCCESS',
         'Data': lambda_update_resp,
         'PhysicalResourceId': lambda_update_resp.get('FunctionArn')
     }
-    _logger.info(json.dumps(cfn_resp))
 
+    # cfn_resource will log the response so this is here only to aide if we
+    # suspect issues communicating back to CFN successfully.
+    _logger.debug('cfn_resp: {}'.format(json.dumps(cfn_resp)))
+
+    # cfn_resource will signal CFN with this.
     return cfn_resp
 
 if __name__ == '__main__':
